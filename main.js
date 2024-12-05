83c71279d3d8fb39fb543b0d632404aa7a0c7afa
Cieliocas <francielioevangelista@hotmail.com>
2024-12-05 03:38:13 -0300

    Teste OO

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const carro = { // Objeto
+const carroDoJoao = { // Objeto
     marca: 'Ford', // atributo
     modelo: 'Fiesta', // atributo
     anoModelo: 2020, // atributo
     anoFabricacao: 2019, // atributo
     acelerar: function() {  // Método
+        console.log('Acelerando...');
+    }
+}
+
+const carroDaMaria = {
+    marca: 'Fiat',
+    modelo: 'Uno',
+    anoModelo: 2019,
+    anoFabricacao: 2018,
+    acelerar: function() {
         console.log('Acelerando...');
     }
 }
