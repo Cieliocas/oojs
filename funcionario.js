48663ccfae910c08a856e2f6c31b68b264fa881b
Cieliocas <francielioevangelista@hotmail.com>
2024-12-09 23:08:20 -0300

    Herança em js

--- funcionario.js
+++ funcionario.js
@@ -3,17 +3,18 @@ function Pessoa(nome) {
     this.dizOi = function() {
         console.log(this.nome + ' diz: Oi!');
     }
+    this.dizCargo = function() {
+        console.log(this.nome + ' é ' + this.cargo);
+    }
 }
 
 function Funcionario(nome, cargo, salario) {
-    this.nome = nome;
     this.cargo = cargo;
     this.salario = salario;
+
+    Pessoa.call(this, nome);
 }
 
-const pessoa1 = new Pessoa('Maria');
-const funcionario1 = new Funcionario('Maria', 'dev front-end', 5000);
-pessoa1.dizOi();
-
-console.log(pessoa1);
-console.log(funcionario1); 
+const funcionario1 = new Funcionario('Maria', 'Dev Front-end', 5000);
+funcionario1.dizOi();
+funcionario1.dizCargo();
