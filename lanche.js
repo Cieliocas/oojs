13849cb7c6465021a3de7e7722ac0cd67db91317
Cieliocas <francielioevangelista@hotmail.com>
2024-12-11 18:30:36 -0300

    Comments

--- lanche.js
+++ lanche.js
@@ -1,3 +1,5 @@
+
+// Classe Comida: Esta é a classe base que contém quatro propriedades: tipo, nome, categoria e ingrediente.
 function Comida(tipo, nome, categoria, ingrediente) {
     this.tipo = tipo;
     this.nome = nome;
@@ -5,24 +7,24 @@ function Comida(tipo, nome, categoria, ingrediente) {
     this.ingrediente = ingrediente;
 }
 
-Comida.prototype.displayInfo = function() {
+Comida.prototype.displayInfo = function() { //Método displayInfo: Um método de exibição que imprime as informações do objeto no console.
     console.log(`Tipo: ${this.tipo} | Nome: ${this.nome} | Categoria: ${this.categoria} | Ingrediente: ${this.ingrediente}`);
 }
-function Lanche(nome, ingrediente) {
+function Lanche(nome, ingrediente) { // Classe Lanche: Herda de Comida usando Comida.call(this, ...) para chamar o construtor da classe Comida.
     Comida.call(this, 'Lanche', nome, 'salgado', ingrediente);
 }
 
 Lanche.prototype = Object.create(Comida.prototype);
 Lanche.prototype.constructor = Lanche;
 
-Lanche.prototype.displayInfo = function() {
+Lanche.prototype.displayInfo = function() { // Método displayInfo: Sobrescreve o método displayInfo para garantir que o método do pai (Comida) seja chamado.
     Comida.prototype.displayInfo.call(this);
 };
-function Bebida(nome, sabor, embalagem) {
+function Bebida(nome, sabor, embalagem) { // Classe Bebida: Similar à classe Lanche, mas usada para bebidas.
     Comida.call(this, 'Bebida', nome, sabor, embalagem);
 }
 
-Bebida.prototype = Object.create(Comida.prototype);
+Bebida.prototype = Object.create(Comida.prototype); // Prototype Chain: Bebida.prototype é configurado para herdar de Comida.prototype.
 Bebida.prototype.constructor = Bebida;
 
 Bebida.prototype.displayInfo = function() {
