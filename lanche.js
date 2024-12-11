052b44ab7273086d2b06f760dd9c917da706ed34
Cieliocas <francielioevangelista@hotmail.com>
2024-12-11 18:18:40 -0300

    Classe Base

--- lanche.js
+++ lanche.js
@@ -1,2 +1,11 @@
+function Comida(tipo, nome, categoria, ingrediente) {
+    this.tipo = tipo;
+    this.nome = nome;
+    this.categoria = categoria;
+    this.ingrediente = ingrediente;
+}
 
-console.log('hello world')
+Comida.prototype.displayInfo = function() {
+    console.log(`Tipo: ${this.tipo} Nome: ${this.nome} Categoria: ${this.categoria} Ingrediente: ${this.ingrediente}`);
+}
+
