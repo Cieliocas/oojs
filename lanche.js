d5dd344d7cc21142a825b06138f5d94059455eb1
Cieliocas <francielioevangelista@hotmail.com>
2024-12-11 18:26:13 -0300

    Test

--- lanche.js
+++ lanche.js
@@ -6,7 +6,7 @@ function Comida(tipo, nome, categoria, ingrediente) {
 }
 
 Comida.prototype.displayInfo = function() {
-    console.log(`Tipo: ${this.tipo} | Nome: ${this.nome} Categoria: ${this.categoria} Ingrediente: ${this.ingrediente}`);
+    console.log(`Tipo: ${this.tipo} | Nome: ${this.nome} | Categoria: ${this.categoria} | Ingrediente: ${this.ingrediente}`);
 }
 function Lanche(nome, ingrediente) {
     Comida.call(this, 'Lanche', nome, 'salgado', ingrediente);
@@ -29,3 +29,18 @@ Bebida.prototype.displayInfo = function() {
     Comida.prototype.displayInfo.call(this);
 };
 
+// test lanches
+const lanche1 = new Lanche('Croissant', 'Queijo');
+const lanche2 = new Lanche('Pastel', 'Carne');
+
+lanche1.displayInfo();
+lanche2.displayInfo();
+
+
+// test bebidas
+const bebida1 = new Bebida('Refrigerante', 'CocaCola', 'Lata');
+const bebida2 = new Bebida('Suco', 'Laranja', 'Copo');
+
+
+bebida1.displayInfo();
+bebida2.displayInfo();
